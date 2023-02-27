7a83a5dcafb9e0a3a2f9996253f19d9b06a39803
Ankit Pal <palankit.ap@gmail.com>
2023-02-27 17:01:57 +0530

    First Program

---
+++ Test.java
@@ -0,0 +1,7 @@
+class Test {
+  public static void main(String arg[]) {
+    System.out.println("Hello!");
+    System.out.println("Welcome to the world of Java.");
+    System.out.println("Let us learn Java.");
+  }
+}
