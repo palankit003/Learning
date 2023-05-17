64b7ec98a9d5443ed273f24ac319c66223f51dca
Ankit Pal <palankit.ap@gmail.com>
2023-05-17 13:03:50 +0530

    cleanup

--- Java/Test.java
+++
@@ -1,8 +0,0 @@
-package Java;
-class Test {
-  public static void main(String arg[]) {
-    System.out.println("Hello!");
-    System.out.println("Welcome to the world of Java.");
-    System.out.println("Let us learn Java.");
-  }
-}
